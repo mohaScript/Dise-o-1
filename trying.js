18767fa58dc2dd3ffe832d385b7b1a5d9f41aeb5
HAMED <hamedhinfo4@gmail.com>
2019-04-09 16:30:08 +0200

    M

---
+++ trying.js
@@ -0,0 +1,6 @@
+function register(){
+    user.name = document.getElementById('input').Value;
+    user.pass = document.getElementById('input').Value;
+    alert('Tu cuenta ha sido creada correctamente');
+    console.log(user.name);
+}
